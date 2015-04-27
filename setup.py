baaf2f6dbf3a9e90c8541edd2f3b35ac41e08e55
Yann Kaiser <kaiser.yann@gmail.com>
2015-04-26 19:39:51 -0700

    Version 3.0b1.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='clize',
-    version='3.0a2',
+    version='3.0b1',
     description='Command-line argument parsing for Python, without the effort',
     license='MIT',
     url='https://github.com/epsy/clize',
