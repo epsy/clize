ce9e381ace65bb44c654b30fac922673088b424a
Chris Angelico <rosuav@gmail.com>
2015-05-08 15:53:55 +1000

    Proposed @cmdline decorator to improve DRY in usage

--- clize/__init__.py
+++ clize/__init__.py
@@ -5,6 +5,6 @@
 """procedurally generate command-line interfaces from callables"""
 
 from clize.parser import Parameter
-from clize.runner import Clize, SubcommandDispatcher, run
+from clize.runner import Clize, SubcommandDispatcher, run, cmdline
 from clize.legacy import clize, make_flag
 from clize.errors import UserError, ArgumentError
