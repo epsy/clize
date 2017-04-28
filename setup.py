4b86d4449fd643cc32170d3144ae83ec85ca0e39
Yann Kaiser <kaiser.yann@gmail.com>
2017-04-28 08:59:22 -0700

    version 4.0.1
    Changes since 4.0:
    
    * Fix literal block not working in Sphinx docstrings
    * Separate test dependencies from install dependencies
    * Add a comparison to Fire in the docs

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='clize',
-    version='4.0',
+    version='4.0.1',
     description='Turn functions into command-line interfaces',
     license='MIT',
     url='https://github.com/epsy/clize',
