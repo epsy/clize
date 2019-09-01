347f8f0de10a220433f9856597f5afeffdc7ca4c
Yann Kaiser <kaiser.yann@gmail.com>
2019-09-01 01:10:32 -0700

    CI: error on warning

--- clize/tests/__init__.py
+++ clize/tests/__init__.py
@@ -1,3 +1,15 @@
 # clize -- A command-line argument parser for Python
 # Copyright (C) 2011-2016 by Yann Kaiser and contributors. See AUTHORS and
 # COPYING for details.
+
+import sys
+import warnings
+
+
+__all__ = []
+
+
+if sys.argv and "test" in sys.argv[0]:
+    warnings.filterwarnings("default")
+    warnings.filterwarnings("error", module="clize")
+    warnings.filterwarnings("error", module=".*/clize/")
