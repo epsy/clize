14e551d5a904ed9ed14705682be43d2cbca98716
Yann Kaiser <y.kaiser@criteo.com>
2021-11-07 20:17:21 -0800

    update RTD config
    - Use `.readthedocs.yaml`
    - move docs requirements to extra_requires
    - require pip 21 to avoid version incompatiblity between sphinx===4.2.0 <>
    docutils===18.0

--- setup.py
+++ setup.py
@@ -32,6 +32,10 @@ setup(
     ],
     extras_require={
         'datetime': ['python-dateutil'],
+        'clize-own-docs': [
+            'sphinx~=4.2.0',
+            'sphinx_rtd_theme',
+        ],
     },
     packages=('clize', 'clize.tests'),
     test_suite='clize.tests',
