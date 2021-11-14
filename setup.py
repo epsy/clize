cdd0989d1cf8e1ca36a6162dcf9b605ad94ec2b2
Yann Kaiser <y.kaiser@criteo.com>
2021-11-13 20:20:16 -0800

    bump version to 5.0.0a1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ test_requirements = [
 
 setup(
     name='clize',
-    version='4.1.1',
+    version='5.0.0a1',
     description='Turn functions into command-line interfaces',
     long_description=long_description,
     long_description_content_type='text/x-rst',
